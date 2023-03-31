d39d4518220653b66b81355ea8f0542c07727ae3
DJ7567 <divyaraj7567@gmail.com>
2023-03-31 04:19:03 +0100

    without gui

--- GSM.py
+++ GSM.py
@@ -1,20 +1,41 @@
 import serial
+import time
+import random
 
-ser = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=1)
-print("started")
+# configure the serial port
 
-
-ser.write(b'AT+CMGF=1\r\n')
-ser.write(b'AT+CMGS="+"918200412217"\r\n')
-ser.write(b'Hello, this is a test message.\r\n')
-ser.write(chr(26).encode())
+ser = serial.Serial('/dev/ttyAMA1', 9600, timeout=1)
 
 
-# read the response
-response = ser.readlines()
-for line in response:
-    print(line.decode('utf-8').strip())
+def GSM_OTP(num):
+	otp=0
+	for i in range(4):
+		otp = otp +random.randint(1,9)
+		otp =otp*10
+	msg='OTP '+str(otp)+'\r\n'
+	strg='AT+CMGS="'+str(num)+'"\r\n'
+	# send an SMS message
+	ser.write('AT\r\n'.encode())
+	time.sleep(1)
+	ser.write('AT+CMGF=1\r\n'.encode())
+	time.sleep(1)
 
+	# set SMS mode to text
+	ser.write(strg.encode())
+	time.sleep(1)# replace PHONE_NUMBER with the recipient's phone number
+	
+	
+	ser.write(msg.encode())
+	time.sleep(1)# replace with your message
+	ser.write(chr(26).encode()) # send the message (Ctrl+Z)
 
-ser.close()
+	# wait for the response
+	response = ser.read(1024)
+
+	# print the response
+	print(response.decode())
+	
+
+GSM_OTP(6353935962)
+
 
